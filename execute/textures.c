d9b75f820296e4d852ac27a167c8ffa301470797
jeandrad1 <jesusandradeperez@gmail.com>
2025-02-01 19:02:01 +0100

    map2, map3, map 4 and maze to make. Everything else is done

--- execute/textures.c
+++ execute/textures.c
@@ -6,7 +6,7 @@
 /*   By: jeandrad <jeandrad@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/13 18:30:26 by jeandrad          #+#    #+#             */
-/*   Updated: 2025/01/28 15:36:14 by jeandrad         ###   ########.fr       */
+/*   Updated: 2025/02/01 18:58:23 by jeandrad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,18 +20,18 @@ void	load_walls(t_game *game, t_element *elem)
 
 	xpm = mlx_load_xpm42(elem->north);
 	if (!xpm)
-		ft_error("texture not found1", game);
+		ft_error("texture north not found", game);
 	game->textures.north = &xpm->texture;
 	xpm = mlx_load_xpm42(elem->south);
 	if (!xpm)
-		ft_error("texture not found2", game);
+		ft_error("texture south not found", game);
 	game->textures.south = &xpm->texture;
 	xpm = mlx_load_xpm42(elem->west);
 	if (!xpm)
-		ft_error("texture not found3", game);
+		ft_error("texture west not found", game);
 	game->textures.west = &xpm->texture;
 	xpm = mlx_load_xpm42(elem->east);
 	if (!xpm)
-		ft_error("texture not found4", game);
+		ft_error("texture east not found", game);
 	game->textures.east = &xpm->texture;
 }
